1e79b3dc7c706a0293fb189894c5b01f8b454824
muellers-saw-leipzig <muellers@saw-leipzig.de>
2020-04-17 13:11:28 +0200

    added behaviour sqlided to every content type added index on attribute sqlid added test vocabulary

---
+++ src/memo/contenttypes/tests/test_vocab_test_vocab.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from memo.contenttypes import _
+from memo.contenttypes.testing import MEMO_CONTENTTYPES_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyTokenized
+
+import unittest
+
+
+class TestVocabIntegrationTest(unittest.TestCase):
+
+    layer = MEMO_CONTENTTYPES_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_vocab_test_vocab(self):
+        vocab_name = 'memo.contenttypes.TestVocab'
+        factory = getUtility(IVocabularyFactory, vocab_name)
+        self.assertTrue(IVocabularyFactory.providedBy(factory))
+
+        vocabulary = factory(self.portal)
+        self.assertTrue(IVocabularyTokenized.providedBy(vocabulary))
+        self.assertEqual(
+            vocabulary.getTerm('sony-a7r-iii').title,
+            _(u'Sony Aplha 7R III'),
+        )
